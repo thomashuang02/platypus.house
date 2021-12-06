8a7446f0830e8afdc3a2a585b8f960a6e5edfbec
Thomas Huang <thomashuang02@gmail.com>
2021-12-06 16:10:32 -0500

    lots of visual changes, added project, dark/light mode

---
+++ js/themeToggle.js
@@ -0,0 +1,12 @@
+function theme_set(toggled) {
+    document.getElementById('style-light').disabled = toggled;
+    document.getElementById('style-dark').disabled = !toggled;
+    localStorage.setItem('theme-toggled', toggled ? '1' : '');
+    document.getElementById('mode').innerHTML = toggled ? '阳' : '阴';
+}
+
+function theme_toggle() {
+    theme_set(!document.getElementById('style-light').disabled);
+}
+
+theme_set(localStorage.getItem('theme-toggled'));
